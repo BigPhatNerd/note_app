d2b5b9ff2183e337a58c1d1bdb879a9f1e314659
Wilson Horrell <wilsonhorrell@gmai.com>
2019-06-24 18:24:42 +0000

    Started communication with the RESTful API back-end.

--- src/App.js
+++ src/App.js
@@ -3,30 +3,39 @@ import './App.css';
 import Nav from './components/Nav';
 import List from './components/List';
 import Note from './components/Note';
+import axios from 'axios';
 
 class App extends Component {
- constructor() {
- 	super();
- 	this.state = {
- 		showNote: false
- 	};
- }
+	constructor() {
+		super();
+		this.state = {
+			showNote: false
+		};
+	}
 
- toggleNote = () => {
- 	this.setState({
- 		showNote: ! this.state.showNote
- 	});
- }
- render() {
- 	const { showNote } = this.state;
-    return (
-      <div className="App">
-          <Nav toggleNote={this.toggleNote} showNote={showNote} />
-        { showNote ? <Note /> : <List /> }
-      </div>
-      );
-  }
-}
+	toggleNote = () => {
+		this.setState({
+			showNote: ! this.state.showNote
+		});
+	}
 
-export default App;
+	getNotes = () => {
+		axios.get('https://note-api-horrell.herokuapp.com/notes')
+		.then((res) => console.log(res.data) )
+		.catch((err) => console.log(err.response.data) );
+	}
+
+
+	render() {
+		const { showNote } = this.state;
+		return (
+			<div className="App">
+			<Nav toggleNote={this.toggleNote} showNote={showNote} />
+			{ showNote ? <Note /> : <List getNotes={this.getNotes} /> }
+			</div>
+			);
+		}
+	}
+
+	export default App;
 
