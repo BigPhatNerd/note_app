ee67e3dc203185dfd7b7b0abf3ee9e8b03e9537c
Wilson Horrell <wilsonhorrell@gmai.com>
2019-06-24 19:41:28 +0000

    Added edit and delete icons to the notes.

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 
-
 class NoteCard extends Component {
-	render() {
-		return (
-			<div className="note-card-container">
-			NoteCard component
-			</div>
-			);
-	}
+  render() {
+    const { note } = this.props;
 
-
+    return (
+      <div className="note-card-container">
+        <div className="note-card-title">
+          {note.title}
+        </div>
+        <div className="note-card-content">
+          {note.content}
+        </div>
+        <span className="note-card-delete">
+          <i className="material-icons">close</i>
+        </span>
+        <span className="note-card-edit">
+        <i className="material-icons">mode_edit</i>
+        </span>
+      </div>
+    );
+  }
 }
 
 export default NoteCard;
