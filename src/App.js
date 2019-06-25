0e19ed8f58c3393d4c4242eccc95c77dac49acf5
Wilson Horrell <wilsonhorrell@gmai.com>
2019-06-25 17:29:10 +0000

    Make a form appear to enter a tag to the note

--- src/App.js
+++ src/App.js
@@ -7,66 +7,73 @@ import axios from 'axios';
 import urlFor from './helpers/urlFor';
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			showNote: false,
-			notes: [],
-			note: {}
-		};
-	}
+    constructor() {
+        super();
+        this.state = {
+            showNote: false,
+            notes: [],
+            note: {},
+            newTag: false
+        };
+    }
 
-	toggleNote = () => {
-		this.setState({
-			showNote: ! this.state.showNote,
-			note: {}
-		});
-	}
+    toggleNote = () => {
+        this.setState({
+            showNote: !this.state.showNote,
+            note: {}
+        });
+    }
 
-	getNotes = () => {
-		axios.get(urlFor('notes'))
-		.then((res) => this.setState({ notes: res.data }) )
-		.catch((err) => console.log(err.response.data) );
-	}
+    getNotes = () => {
+        axios.get(urlFor('notes'))
+            .then((res) => this.setState({ notes: res.data }))
+            .catch((err) => console.log(err.response.data));
+    }
 
-	getNote = (id) => {
-		axios.get(urlFor(`notes/${id}`))
-		.then((res) => this.setState({ note: res.data, showNote: true }) )
-		.catch((err) => console.log(err.response.data) );
-	}
+    getNote = (id) => {
+        axios.get(urlFor(`notes/${id}`))
+            .then((res) => this.setState({ note: res.data, showNote: true }))
+            .catch((err) => console.log(err.response.data));
+    }
 
-	performSubmissionRequest = (data, id) => {
-		if (id) {
-			return axios.patch(urlFor(`notes/${id}`), data);
+    performSubmissionRequest = (data, id) => {
+        if (id) {
+            return axios.patch(urlFor(`notes/${id}`), data);
 
-		} else {
-			return axios.post(urlFor('notes'), data);
-		}
-	}
+        } else {
+            return axios.post(urlFor('notes'), data);
+        }
+    }
 
-	submitNote = (data, id) => {
-		this.performSubmissionRequest(data, id)
-		.then((res) => this.setState({ showNote: false }) )
-		.catch((err) => console.log(err.response.data) );
-	}
+    submitNote = (data, id) => {
+        this.performSubmissionRequest(data, id)
+            .then((res) => this.setState({ showNote: false }))
+            .catch((err) => console.log(err.response.data));
+    }
 
-	deleteNote = (id) => {
-		const newNotesState = this.state.notes.filter((note) => note.id !== id);
-		axios.delete(urlFor(`notes/${id}`))
-		.then((res) => this.setState({ notes: newNotesState }))
-		.catch((err) => console.log(err.response.data) );
-	}
+    deleteNote = (id) => {
+        const newNotesState = this.state.notes.filter((note) => note.id !== id);
+        axios.delete(urlFor(`notes/${id}`))
+            .then((res) => this.setState({ notes: newNotesState }))
+            .catch((err) => console.log(err.response.data));
+    }
+
+    showTagForm = () => {
+        this.setState({ newTag: true });
+    }
 
 
-	render() {
-		const { showNote, notes, note } = this.state;
-		return (
-			<div className="App">
+    render() {
+        const { showNote, notes, note, newTag } = this.state;
+        return (
+            <div className="App">
 			<Nav toggleNote={this.toggleNote} showNote={showNote} />
 			{ showNote ? 
 				<Note
 				note={note}
 				submitNote={this.submitNote}
+				showTagForm={this.showTagForm}
+				newTag={newTag}
 				/> 
 				
 				: 
@@ -78,9 +85,8 @@ class App extends Component {
 				/> 
 			}
 			</div>
-			);
-		}
-	}
+        );
+    }
+}
 
-	export default App;
-
+export default App;
